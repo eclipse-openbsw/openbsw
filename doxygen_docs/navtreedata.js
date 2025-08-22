fdf91c59f7d76a34dde063f478046aa80f731ab5
mthiede-acn2 <mthiede-acn2@users.noreply.github.com>
2025-08-22 15:00:23 +0000

    deploy: 0c79c0ed89eb6763bed50ea31da6eb41ce280498

---
+++ doxygen_docs/navtreedata.js
@@ -0,0 +1,79 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Eclipse OpenBSW", "index.html", [
+    [ "Deprecated List", "da/d58/deprecated.html", null ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d1/d59/CANFrame_8h.html",
+"d2/d05/classcan_1_1AbstractStaticBitFieldFilter.html#a60e313e1136ebe54ebf85711f5a64378",
+"d2/dfd/classio_1_1SplitWriter.html#a9d478bbaa6b7adf3c89c33a464da2c9d",
+"d4/d16/classsafeMonitor_1_1Sequence.html",
+"d5/d27/classutil_1_1string_1_1ConstString.html#a93de6a5252c4672ca312bba25ffa9787",
+"d6/dbd/classutil_1_1format_1_1PrintfFormatter.html",
+"d7/de0/classlogger_1_1IEntryFormatter.html#a3cad488a49b22ce16531c5628bfeefe6",
+"d8/de5/classlifecycle_1_1ComponentB.html",
+"d9/dfd/assert_8cpp.html#a0e185175e8441a02f55ff62e084287f6",
+"da/dd4/classutil_1_1memory_1_1BuddyMemoryManager_1_1AcquireResult.html#aa861bdfaa9130c25979abb737bbe05a7",
+"db/d95/classlogger_1_1ComponentConfig.html#a77f04e108b762af96769c3ef88927886",
+"dc/d14/classasync_1_1internal_1_1TaskImpl.html#ac4b3dad5d07a0cfed8780ef67bd83718",
+"dc/dc8/classcan_1_1ICanTransceiver.html#a7575e963c25b309d9139c0af861d250a",
+"dd/d8c/classtransport_1_1TransportMessage.html#a82c3912d38bd68243464e8d5e0b7a7f0",
+"de/d91/classuds_1_1SubfunctionWithAuthenticationAndSessionControl.html#a314dacdb9140ceeec7e9a37690102e6c",
+"df/d94/classlifecycle_1_1LifecycleManager.html#a180f4462975438f6a19292c630f01c7b"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
