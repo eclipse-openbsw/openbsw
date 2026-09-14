3615bf7b14e147f9c3cd57f9a2c6858425158ca3
danielsbsantos <daniel.sb.santos@ctw.bmwgroup.com>
2026-09-14 15:42:51 +0000

    feat(routing): Add routing PDU <-> CAN Frame conversion methods

--- libs/bsw/routing/src/routing/pduRouting.cpp
+++ libs/bsw/routing/src/routing/pduRouting.cpp
@@ -10,6 +10,8 @@
 
 #include "routing/pduRouting.h"
 
+#include <can/canframes/CANFrame.h>
+
 #include "routing/Logger.h"
 
 #include <blob/Config.h>
@@ -20,6 +22,51 @@
 namespace routing
 {
 namespace logger = ::util::logger;
+
+::etl::span<uint8_t> canFrameToPdu(::can::CANFrame const& frame, ::etl::span<uint8_t> buffer)
+{
+    uint32_t const payloadLength = frame.getPayloadLength();
+    if ((payloadLength > ::can::CANFrame::MAX_FRAME_LENGTH)
+        || (buffer.size() < (ROUTING_PDU_HEADER_SIZE + payloadLength)))
+    {
+        return {};
+    }
+
+    ::etl::be_uint32_t const messageId{frame.getId()};
+    ::etl::be_uint32_t const parsedPayloadLength{payloadLength};
+    etl::copy_n(messageId.data(), ROUTING_PDU_MESSAGE_ID_SIZE, buffer.begin());
+    etl::copy_n(
+        parsedPayloadLength.data(),
+        ROUTING_PDU_PAYLOAD_LENGTH_SIZE,
+        buffer.subspan(ROUTING_PDU_MESSAGE_ID_SIZE).begin());
+    etl::copy_n(frame.getPayload(), payloadLength, buffer.subspan(ROUTING_PDU_HEADER_SIZE).begin());
+
+    return buffer.first(ROUTING_PDU_HEADER_SIZE + payloadLength);
+}
+
+bool pduToCanFrame(::etl::span<uint8_t const> buffer, ::can::CANFrame& frame)
+{
+    if (buffer.size() < ROUTING_PDU_HEADER_SIZE)
+    {
+        return false;
+    }
+
+    uint32_t const payloadLength = ::etl::be_uint32_t(
+        buffer.subspan(ROUTING_PDU_MESSAGE_ID_SIZE, ROUTING_PDU_PAYLOAD_LENGTH_SIZE).data());
+    if ((payloadLength > ::can::CANFrame::MAX_FRAME_LENGTH)
+        || (buffer.size() < (ROUTING_PDU_HEADER_SIZE + payloadLength)))
+    {
+        return false;
+    }
+
+    uint32_t const messageId
+        = ::etl::be_uint32_t(buffer.subspan(0U, ROUTING_PDU_MESSAGE_ID_SIZE).data());
+    frame = ::can::CANFrame{
+        messageId,
+        buffer.subspan(ROUTING_PDU_HEADER_SIZE).data(),
+        static_cast<uint8_t>(payloadLength)};
+    return true;
+}
 
 // NOLINTBEGIN(cppcoreguidelines-pro-type-vararg): Logger API uses C-style varargs.
 
---
+++ libs/bsw/routing/test/src/routing/CanFramePduConversionTest.cpp
@@ -0,0 +1,67 @@
+/********************************************************************************
+ * Copyright (c) 2026 BMW AG
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
+#include "routing/pduRouting.h"
+
+#include <can/canframes/CANFrame.h>
+
+#include <etl/array.h>
+#include <gtest/gtest.h>
+
+namespace
+{
+TEST(CanFramePduConversionTest, serializesAndDeserializesCanFrame)
+{
+    ::etl::array<uint8_t, ::can::CANFrame::MAX_FRAME_LENGTH> payload{1U, 2U, 3U, 4U};
+    ::can::CANFrame const input{0x123U, payload.data(), 4U};
+    ::etl::array<uint8_t, 16U> buffer{};
+
+    auto const serialized = ::routing::canFrameToPdu(input, ::etl::make_span(buffer));
+
+    ASSERT_EQ(12U, serialized.size());
+    ::can::CANFrame output;
+    ASSERT_TRUE(::routing::pduToCanFrame(serialized, output));
+    EXPECT_EQ(input, output);
+}
+
+TEST(CanFramePduConversionTest, rejectsBufferTooSmallForSerialization)
+{
+    ::can::CANFrame const input{0x123U};
+    ::etl::array<uint8_t, ::routing::ROUTING_PDU_HEADER_SIZE - 1U> buffer{};
+
+    EXPECT_TRUE(::routing::canFrameToPdu(input, ::etl::make_span(buffer)).empty());
+}
+
+TEST(CanFramePduConversionTest, rejectsIncompleteHeader)
+{
+    ::etl::array<uint8_t, ::routing::ROUTING_PDU_HEADER_SIZE - 1U> buffer{};
+    ::can::CANFrame output;
+
+    EXPECT_FALSE(::routing::pduToCanFrame(::etl::make_span(buffer), output));
+}
+
+TEST(CanFramePduConversionTest, rejectsPayloadLengthLargerThanCanFrame)
+{
+    ::etl::array<uint8_t, ::routing::ROUTING_PDU_HEADER_SIZE> buffer{};
+    buffer[7] = static_cast<uint8_t>(::can::CANFrame::MAX_FRAME_LENGTH + 1U);
+    ::can::CANFrame output;
+
+    EXPECT_FALSE(::routing::pduToCanFrame(::etl::make_span(buffer), output));
+}
+
+TEST(CanFramePduConversionTest, rejectsBufferShorterThanDeclaredPayload)
+{
+    ::etl::array<uint8_t, ::routing::ROUTING_PDU_HEADER_SIZE + 1U> buffer{};
+    buffer[7] = 2U;
+    ::can::CANFrame output;
+
+    EXPECT_FALSE(::routing::pduToCanFrame(::etl::make_span(buffer), output));
+}
+} // namespace
